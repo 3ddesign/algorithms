db7449e552efa28a68c8ce8926c29edc4a2cc7ec
Dmitry <3ddesign@bk.ru>
2021-04-30 09:10:05 +0300

    add factorial examlpe, sipmle solution with TC 0(n)

--- algorithms_fundamentals/examples.js
+++ algorithms_fundamentals/examples.js
@@ -59,3 +59,18 @@ function isPowerOfTwo(number) {
 
 console.log(isPowerOfTwo(8));
 console.log(isPowerOfTwo(5));
+
+/* Example 4 */
+
+function fact(number) {
+    let result = 1;
+    for (let i = 2; i <= number; i++) {
+        result = result * i;
+    }
+    return result;
+}
+
+// 0(n)
+
+console.log(fact(3));
+console.log(fact(4));

042345760c718a606534445d8dbbb8e041b310bc
Dmitry <3ddesign@bk.ru>
2021-05-15 09:23:24 +0300

    add quicksort example

--- algorithms_fundamentals/sorting.js
+++ algorithms_fundamentals/sorting.js
@@ -22,5 +22,43 @@ function bubleSort(array) {
     return result;
 }
 
-
 console.log(bubleSort([3, 9, -3, 20, 1, 33]));
+
+// Best case: O(n)
+// Average case: O(n^2)
+// Worst case: O(n^2)
+
+/* Example 1: Quicksort */
+
+function quicksort(array) {
+    const copiedArray = [...array];
+
+    if (copiedArray.length <= 1) {
+        return copiedArray;
+    }
+
+    const smallerElemetsArray = [];
+    const biggerElemetsArray = [];
+    const pivotElement = copiedArray.shift();
+    const centerElemetsArray = [pivotElement];
+
+    while (copiedArray.length) {
+        const currentElement = copiedArray.shift();
+        if (currentElement === pivotElement) {
+            centerElemetsArray.push(currentElement);
+        } else if (currentElement < pivotElement) {
+            smallerElemetsArray.push(currentElement);
+        } else {
+            biggerElemetsArray.push(currentElement);
+        }
+    }
+
+    const smallerElementsSortedArray = quicksort(smallerElemetsArray);
+    const biggerElementsSortedArray = quicksort(biggerElemetsArray);
+
+    return smallerElementsSortedArray.concat(centerElemetsArray, biggerElementsSortedArray);
+}
+
+
+
+console.log(quicksort([3, 9, -3, 20, 1, 33]));

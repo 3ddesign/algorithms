fa5decc0c8274df7aa970946f737ea740b2caa8b
Dmitry <3ddesign@bk.ru>
2021-05-17 20:27:21 +0300

    add merge sort example

--- algorithms_fundamentals/sorting.js
+++ algorithms_fundamentals/sorting.js
@@ -28,7 +28,7 @@ console.log(bubleSort([3, 9, -3, 20, 1, 33]));
 // Average case: O(n^2)
 // Worst case: O(n^2)
 
-/* Example 1: Quicksort */
+/* Example 2: Quicksort */
 
 function quicksort(array) {
     const copiedArray = [...array];
@@ -64,3 +64,44 @@ function quicksort(array) {
 // Algorithm runtime: O(n^logb(a) * log n) => O(n * log n)
 
 console.log(quicksort([3, 9, -3, 20, 1, 33]));
+
+
+/* Example 3: Megre Sort */
+
+function mergeSort(array) {
+
+    if (array.length < 2) {
+        return array;
+    } 
+
+    if (array.length === 2) {
+        return array[0] > array[1] ? [array[1], array[0]] : array;
+    }
+
+    const middle = Math.floor(array.length / 2);
+    const leftArray = array.clice(0, middle);
+    const rightArray = array.clice(middle);
+
+    const leftSortedArray = mergeSort(leftArray);
+    const rightSortedArray = mergeSort(rightArray);
+
+    const mergedArray = [];
+    let leftArrayIndex = 0;
+    let rightArrayIndex = 0;
+    while (leftArrayIndex < rightArrayIndex.length ||
+           rightArrayIndex < rightSortedArray.length) { 
+
+            if (leftArrayIndex >= leftSortedArray.length ||
+                leftSortedArray[leftArrayIndex] > rightSortedArray[rightArrayIndex]) {
+                mergedArray.push(rightSortedArray[rightArrayIndex]);
+                rightArrayIndex++;
+            } else {
+                mergedArray.push(leftSortedArray[leftArrayIndex]);
+                leftArrayIndex++;
+            }
+    }    
+
+    return mergedArray;
+}
+
+console.log(mergeSort([3, 9, -3, 20, 1, 33]));

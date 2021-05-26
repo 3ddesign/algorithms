e18045164f04393d8286a9799f3e51cd379eacfb
Dmitry <3ddesign@bk.ru>
2021-05-26 09:23:26 +0300

    expand CPA

--- algorithms_fundamentals/array.js
+++ algorithms_fundamentals/array.js
@@ -3,18 +3,32 @@
 function cartProduct(setA, setB) {
     const product = [];
     
-    for (const setAElemet of setA) {
+    for (let setAElemet of setA) {
+        if (!Array.isArray(setAElemet)) {
+            setAElemet = [setAElemet];
+        }
+
         for (const setBElemet of setB) {
-            product.push([setAElemet, setBElemet]);
+            product.push([...setAElemet, setBElemet]);
         }
     }
 
     return product;
 }
 
+function cartesian(...sets) {
+    let tempProduct = sets[0];
+
+    for (let i = 1; i < sets.length; i++) {
+        tempProduct = cartProduct(tempProduct, sets[i])
+    }
+}
+
 const colors = ['blue', 'red'];
 const sizes = ['m', 'l'];
+const styles = ['round neck', 'v neck'];
 console.log(cartProduct(colors, sizes));
+console.log(cartesian(colors, sizes, styles));
 
-// Time Complexity: O(n*m), O(n^2)
-// Space Complexity: O(n*m), O(n^2)
+// Time Complexity: O(n^x)
+// Space Complexity: O(n^x)

213601e52a916ff061f464d9d28aa47412e0e58d
Dmitry <3ddesign@bk.ru>
2021-05-30 23:02:39 +0300

    add example permutations with repetitions

--- algorithms_fundamentals/array.js
+++ algorithms_fundamentals/array.js
@@ -73,3 +73,30 @@ function getPermutations(options) {
   console.log(getPermutations(todoListItems));
   
   // Time Complexity: O(n!) (for example: 4 * 3 * 2 * 1 = 24 or 5 * 4 * 3 * 2 * 1 = 120)
+
+  /* Example 3: Permutations with Repetitions */
+
+  function getPermutations(options, length) {
+    const permutations = [];
+  
+    if (length === 1) {
+      return options.map(option => [option]);
+    }
+  
+    const partialPermutations = getPermutations(options, length - 1);
+  
+    for (const option of options) {
+      for (const existingPermutation of partialPermutations) {
+        permutations.push([option].concat(existingPermutation));
+      }
+    }
+  
+    return permutations;
+  }
+  
+  const digits = [1, 2, 3, 4];
+  const resultLength = 3;
+  
+  console.log(getPermutations(digits, resultLength));
+  
+  // Time Complexity: O(n^r) => n is the number options, r is the length

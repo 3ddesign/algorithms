f41c3af71652a30301460f6507ef2010816414fb
Dmitry <3ddesign@bk.ru>
2021-06-07 08:49:59 +0300

    add change making algorithm with brute force approach

--- algorithms_fundamentals/solving.js
+++ algorithms_fundamentals/solving.js
@@ -108,3 +108,30 @@ function computeChange(coins, amount) {
 
   return calculatedChange;
 }
+
+function computeChangeBruteForce(coins, amount) {
+  const results = [];
+  for (let i = 0; i < coins.length; i++) {
+    results.push(computeChange(coins.slice(i), amount));
+  }
+
+  let smallestAmountOfCoins = Number.MAX_SAFE_INTEGER;
+  let finalResult;
+  for (const result of results) {
+    if (result.numberOfCoins < smallestAmountOfCoins) {
+      smallestAmountOfCoins = result.numberOfCoins;
+      finalResult = result;
+    }
+  }
+
+  return finalResult;
+}
+
+const availableCoins = [8, 6, 5, 1];
+const targetAmount = 63;
+
+const change = computeChangeBruteForce(availableCoins, targetAmount);
+console.log(change);
+
+// Time Complexity (Greedy Solution): O(n)
+// Time Complexity (Brute Force): O(n^2)
